8854f7eae037f661e8f612b5f76be973650ef63c
Thais Anchieta <thaisregina.af@hotmail.com>
2022-01-06 18:42:01 -0300

    feat: renderização da página

---
+++ render/README.md
@@ -0,0 +1,75 @@
+<h1 align="center">
+  <img src="https://github.com/lenamsst/desafiojs_agencia-cronos-adm/blob/main/assets/banner-marketing.png"/>
+</h1>
+
+<p align="center">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/lenamsst/desafiojs_agencia-cronos-adm">
+  
+  
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/lenamsst/desafiojs_agencia-cronos-adm">
+  
+  <img alt="license" src="https://img.shields.io/github/license/lenamsst/desafiojs_agencia-cronos-adm">
+
+
+</p>
+
+<h1 align="center">CRONOS Agência</h1>
+
+## 💻 Sobre o projeto
+
+Agência Cronos nos contratou para o desenvolvimento do site institucional, o foco é no gerenciamento de serviços.
+
+- Desenvolvimento das funcionalidades para criar, editar e deletar serviços da lista (o famoso CRUD).
+
+Projeto desenvolvido durante o Gama Experience XP39 oferecida pela  <a href="https://www.gama.academy/gama-experience/desenvolvimento-full-stack">[Gama Academy]</a>
+
+## 🎨 Layout
+
+<p align="center" style="display: flex; align-items: flex-start; justify-content: center;">
+  <img alt="gamaacademy" title="#gamaacademy" src="./assets/principal.png" width="400px">
+
+  <img alt="gamaacademy" title="#gamaacademy" src="./assets/cadastrar.png" width="400px">
+  
+  <img alt="gamaacademy" title="#gamaacademy" src="./assets/editar.png" width="400px"> 
+</p>
+
+## 🛠 Tecnologias
+
+As seguintes ferramentas foram usadas na construção do projeto:
+
+- HTML5
+- CSS3
+- JAVASCRIPT
+- REACT
+
+## 🚀 Como executar o projeto
+
+Pré-requisitos
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas: Git, Node.js. Além disto é bom ter um editor para trabalhar com o código como VSCode
+
+
+### 🧭 Rodando a aplicação web
+
+```bash
+# Clone este repositório
+$ git clone https://github.com/lenamsst/desafiojs_agencia-cronos-adm.git
+
+# Acesse a pasta do projeto no seu terminal/cmd
+$ cd desafiojs_agencia-cronos-adm
+
+# Instale as dependências
+$ npm install
+
+# Execute a aplicação em modo de desenvolvimento
+$ npm run start
+
+# A aplicação será aberta na porta:3000 - acesse http://localhost:3000
+```
+
+## 😯 Como contribuir para o projeto
+
+1. Faça um **fork** do projeto.
+2. Crie uma nova branch com as suas alterações: `git checkout -b my-feature`
+3. Salve as alterações e crie uma mensagem de commit contando o que você fez: `git commit -m "feature: My new feature"`
+4. Envie as suas alterações: `git push origin my-feature`
+> Caso tenha alguma dúvida confira este [guia de como contribuir no GitHub](https://github.com/firstcontributions/first-contributions)
---
+++ render/public/script.js
@@ -0,0 +1,150 @@
+const servicos = [{
+    id: 1,
+    nome: 'Desenvolvimento Web',
+    imagem: 'imagens/ilustra-web.png',
+    descricao: 'Consequatur debitis ipsa numquam illum placeat quod deleniti.',
+    acoes: []
+
+}, {
+    id: 2,
+    nome: 'Marketing Digital',
+    imagem: 'imagens/ilustra-marketing.png',
+    descricao: 'Consequatur debitis ipsa numquam illum placeat quod deleniti.',
+    acoes: []
+}, {
+    id: 3,
+    nome: 'Consultoria UX',
+    imagem: 'imagens/ilustra-ux.png',
+    descricao: 'Consequatur debitis ipsa numquam illum placeat quod deleniti.',
+    acoes: []
+}];
+
+
+//Função para cadastrar novo serviço
+function btnCadastrarNovo() {
+    
+    //Captura campos do formulario
+    let nome_input = document.querySelector('#nome');
+    let imagem_input = document.querySelector('#imagem');
+    let descricao_input = document.querySelector('#descricao');
+
+    //Condicional verifica campos vazios e retorna msg de erro
+    if ((nome_input.value == '') || (imagem_input.value == '') || (descricao_input.value == '')) {
+        alert('Nome, Imagem e Descrição devem ser preenchidos!')
+    } else {
+        //Adicionando novo serviço
+        let salvarNovo = {
+            id: servicos.length + 1,
+            nome: nome_input.value,
+            imagem: imagem_input.value,
+            descricao: descricao_input.value
+        }
+
+        alert('Novo serviço adicionado!')
+        servicos.push(salvarNovo);
+        window.scrollTo(0,document.body.scrollHeight)
+        
+    }
+
+      listarServicos();
+      btnLimpar();
+}
+
+const preview = document.getElementById('previewimg');
+preview.style.display = "none";
+
+//Função para pré-visualizar imagem no formulario
+function visualizaImagem (event) {
+    preview.style.display = "block"
+    preview.src = imagem.value
+};
+
+//Função para listar os serviços de forma dinamica
+function listarServicos() {
+
+    let htmlServicos = "";
+
+    //Criando lista de serviço em html
+    for (let i = 0; i < servicos.length; i++) {
+        htmlServicos += `
+            <form>
+            <tr>
+            <td>${servicos[i].nome}</td>            
+            <td><img src="${servicos[i].imagem}"/></td>
+            <td>${servicos[i].descricao}</td>
+            <td>
+              <button class="btn btn-secondary m-1" data-toggle="modal" data-target="#exampleModalCenter" onclick="abrirEdicaoServico(${servicos[i].id})">editar</button>
+              <button class="btn btn-danger m-1" onclick="deletarServico(${servicos[i].id})">excluir</button>
+            </td>
+            </tr>    
+            </form>   
+        `;
+    }
+
+    //Selecionando elemento que vai englobar a lista
+    let painelServicos = document.querySelector('.painel-servicos');
+
+    //Inserindo lista de serviços no html
+    painelServicos.innerHTML = htmlServicos;
+    
+}
+
+//Função para limpar todos os campos do formulario 
+function btnLimpar() {
+    
+    btnEditar.style.display ='none';
+
+    document.getElementById('nome').value = "";
+    document.getElementById('imagem').value = "";
+    document.getElementById('previewimg').src = "";
+    document.getElementById('descricao').value = "";
+}
+
+//Função para abrir a janela modal com dados inseridos nos campos do formúlario
+function abrirEdicaoServico(id) {
+
+    btnSalvo.style.display = 'none';
+    btnEditar.style.display ='initial';
+
+    for(let i = 0; i < servicos.length; i++) {
+        if (servicos[i]['id'] == id){
+            document.getElementById('id').value = servicos[i]['id'];
+            document.getElementById('nome').value = servicos[i]['nome'];
+            document.getElementById('imagem').value = servicos[i]['imagem'];
+            document.getElementById('descricao').value = servicos[i]['descricao'];
+
+        }
+    }
+}
+
+function atualizarServico() { 
+
+    let id = document.getElementById('id').value;
+
+    for(let i = 0; i < servicos.length; i++) {
+        if (servicos[i]['id'] == id){
+
+            let salvarNovo = {
+                id: document.getElementById('id').value,
+                nome: document.getElementById('nome').value,
+                imagem: document.getElementById('imagem').value,
+                descricao: document.getElementById('descricao').value
+            }
+            servicos.splice(i, 1, salvarNovo);
+        }
+    }
+    listarServicos();
+}
+
+function deletarServico (id) {  
+      
+       for(let i = 0; i < servicos.length; i++) {        
+          if (servicos[i]['id'] == id){
+              servicos.splice(i, 1);
+          }        
+      }
+      listarServicos();
+  }
+   
+  
+  listarServicos();
